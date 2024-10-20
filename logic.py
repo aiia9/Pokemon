1b359b28522ca8a64aaabca896414b44567b4562
aiia9 <150587154+aiia9@users.noreply.github.com>
2024-10-20 18:04:07 +1000

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,5 +1,7 @@
 from random import randint
 import requests
+from datetime import datetime, timedelta
+coins = 0
 
 class Pokemon:
     pokemons = {}
@@ -11,8 +13,24 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.hp = randint(200,400)
+        self.power = randint(30,60)
+        self.last_feed_time = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+        
 
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -37,11 +55,42 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покемона: {self.name}"
+        return f"""Имя твоего покемона: {self.name}
+Здоровье покемона: {self.hp}
+Сила покемона: {self.power}
+"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"  
 
+    def chances(self):
+        if self.pokemon_number > 900:
+            return f"Тебе выпал редкий покемон! Ты получаешь 500 монет!"
+        else:
+            return f"Обычный покемон"
 
+class Wizard(Pokemon):
+    def feed(self):
+        super().feed(feed_interval=10)
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+
+    def feed():
+        super().feed(hp_increase=20)
